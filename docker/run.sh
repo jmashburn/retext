d3d39e1056fc6e3d08e3a0c3c580e074f1abb2e2
Jared Mashburn <jmashburn@redhat.com>
2015-03-16 14:29:19 -0400

    Updating

--- docker/run.sh
+++ docker/run.sh
@@ -5,5 +5,14 @@
 
 cd  /var/www/ && composer install
 
+cp /var/www/config/database.ini.mysql /var/www/config/database.ini
+
+# Configure App
+sed -i 's/MYSQL_HOST/'$DATABASE_SERVICE_HOST'/g' /var/www/config/database.ini
+sed -i 's/MYSQL_DATABASE/'$MYSQL_DATABASE'/g' /var/www/config/database.ini
+sed -i 's/MYSQL_PORT/'$DATABASE_SERVICE_PORT'/g' /var/www/config/database.ini
+sed -i 's/MYSQL_USERNAME/root/g' /var/www/config/database.ini
+sed -i 's/MYSQL_PASSWORD/'$MYSQL_ROOT_PASSWORD'/g' /var/www/config/database.ini
+
 #/mysql_user.sh
 exec supervisord -n
