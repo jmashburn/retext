f168ccfd8a82ad3535d5cb8876f8825c1c69d0c0
Jared Mashburn <jmashburn@iwobble.com>
2016-04-11 16:17:50 -0400

    Removing Twilio Tokens

--- module/Retext/config/autoload/local.php
+++ module/Retext/config/autoload/local.php
@@ -2,8 +2,8 @@
 
 return array(
 	'twilio' => array(
-		'sid' => 'ACaad312dd71b3ad7756bf33c18b2652ac',
-		'token' => '5253300491b4f9cbf9e1f69c9651e2bc',
-		'number' => '5129942920'
+		'sid' => $_ENV['TWILIO_SID'],
+		'token' => $_ENV['TWILIO_TOKEN'],
+		'number' => $_ENV['TWILIO_NUMBER']
 	)
 );
