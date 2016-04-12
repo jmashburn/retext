2b18d8e5b9a5314369f96e5e232cce4669885589
Jared Mashburn <jmashburn@iwobble.com>
2016-04-12 16:50:53 -0400

    Update gui_sqlite_create_database.sql

--- var/install/sql/gui_sqlite_create_database.sql
+++ var/install/sql/gui_sqlite_create_database.sql
@@ -72,7 +72,6 @@ CREATE TABLE "gui_users" (
 	 "email" varchar,
 	 "role" varchar
 );
-# password
 INSERT INTO "gui_users" VALUES (1, 'u_9d9dxf9d9sf', 'admin', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'admin@local.dev', 'root');
 
 
