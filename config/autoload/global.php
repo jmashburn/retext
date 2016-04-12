947654f923a8fb9742c9a5b917826fb6cfb97157
Jared Mashburn <jmashburn@iwobble.com>
2016-04-12 09:45:26 -0400

    Update global.php

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -4,7 +4,7 @@ return array(
 	'base_dir' => dirname(dirname(dirname(__FILE__))),
 	'view_paths' => glob('{module/*/views/,module/*/views/*,views/*,views/}/', GLOB_BRACE),
 	'config_dir' => glob('{module/*/config/,config/,tests/config/}', GLOB_BRACE),
-	'db_dir' =>  dirname(dirname(dirname(__FILE__))) . '/var/db',
+	'db_dir' =>  (getenv('TMP_DIR')?getenv('TMP_DIR'):dirname(dirname(dirname(__FILE__))) . '/var/db'),
 	'log_dir' =>  dirname(dirname(dirname(__FILE__))) . '/var/log',
 	'install_dir' => dirname(dirname(dirname(__FILE__))) . '/var/install/sql',
 );
